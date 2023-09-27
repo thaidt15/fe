614871f8adc57676c8a3a309e798c06739049162
Your Name <you@example.com>
2023-09-27 17:00:51 +0700

    update

--- src/compornent/router/router.jsx
+++ src/compornent/router/router.jsx
@@ -4,12 +4,16 @@ import {  Routes, Route } from 'react-router-dom';
 import Homes from '../page/Home.jsx'; 
 import Login from '../page/LoginPage.jsx'; 
 import Class from '../page/ClassPage.jsx'; 
+import RoleList from '../page/RoleList.jsx'; 
+import CheckList from '../page/Checklist.jsx'; 
 const MyRoutes = () => { // Rename the component here
   return (
     <Routes>
       <Route path="/" exact element={<Homes />} /> 
       <Route path="/login" exact element={<Login />} /> 
       <Route path="/class" exact element={<Class />} /> 
+      <Route path="/role-list" exact element={<RoleList />} /> 
+      <Route path="/checklist-list" exact element={<CheckList />} /> 
     </Routes>
   );
 };
